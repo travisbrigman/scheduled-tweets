2c7a9505becc31b0229669fc544ecbe26f286470
travisbrigman <travis1000@gmail.com>
2021-06-09 10:36:00 -0500

    adds user model and validations

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+# email:string
+# password_digest:string
+
+# password:string VIRTUAL
+# password_confirmation:string VIRTUAL
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address"}
+end
