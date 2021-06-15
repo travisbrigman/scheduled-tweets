978833612a018ef2490df28e21dd7736f19a0541
travisbrigman <travis1000@gmail.com>
2021-06-15 08:06:17 -0500

    twitter auth works again

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user.present? && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_path, notice: "Logged In Successfully"
+      redirect_to root_path, notice: 'Logged In Successfully'
     else
       flash[:alert] = 'Invalid Email or Password'
       render :new
@@ -16,6 +16,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_path, notice: "Logged Out"
+    redirect_to root_path, notice: 'Logged Out'
   end
 end
