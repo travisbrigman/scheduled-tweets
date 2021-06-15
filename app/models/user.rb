5be77fee6da8da44eb1fc7a9d228a1d401d19ba9
travisbrigman <travis1000@gmail.com>
2021-06-15 05:47:31 -0500

    adds twitter auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@
 # password:string VIRTUAL
 # password_confirmation:string VIRTUAL
 class User < ApplicationRecord
+  has_many :twitter_accounts, class_name: "twitter_account", foreign_key: "reference_id"
   has_secure_password
-
+ 
   validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'must be a valid email address'}
 end
